656df31ebdafbc08015c86e03008d20c039a1e45
Marek Laz <mareklaz4@gmail.com>
2022-10-13 23:27:32 +0200

    routes

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ require("./config/db.config");
 
 const app = express();
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Headers", "content-type");
+  res.set("Access-Control-Allow-Methods", "*");
+  res.set("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(logger("dev"));
 app.use(express.json()); // Nos permite usar el req.body
 
