a5080fd790c56df7901ee288d9b9ae4915085e6e
Marek Laz <mareklaz4@gmail.com>
2022-10-13 18:40:17 +0200

    ...

--- routes/router.config.js
+++ routes/router.config.js
@@ -1,24 +1,23 @@
-const router = require('express').Router();
-const userController = require('../controllers/user.controller');
-const projectController = require('../controllers/project.controller');
+const router = require("express").Router();
+const userController = require("../controllers/user.controller");
+const projectController = require("../controllers/project.controller");
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.json({ ok: true });
-})
-
+});
 
 // Users
 
-router.get('/users', userController.list);
-router.post('/users', userController.create);
-router.get('/users/:id', userController.detail);
-router.delete('/users/:id', userController.delete);
+router.get("/users", userController.list);
+router.post("/users", userController.create);
+router.get("/users/:id", userController.detail);
+router.delete("/users/:id", userController.delete);
 
 // Projects
 
-router.get('/projects', projectController.list);
-router.post('/projects', projectController.create);
-router.get('/projects/:id', projectController.detail);
-router.delete('/projects/:id', projectController.delete);
+router.get("/projects", projectController.list);
+router.post("/projects", projectController.create);
+router.get("/projects/:id", projectController.detail);
+router.delete("/projects/:id", projectController.delete);
 
 module.exports = router;
